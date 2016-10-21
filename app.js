9817ae220a62336a61007d8e4f95f50f57db8a4c
ssowemim <ssowemim@ualberta.ca>
2016-10-21 16:32:04 -0600

    pushing changes

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to grab the weather for',
+      string: true
+    }
+})
+.help()
+.alias('help', 'h');
+.argv;
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
