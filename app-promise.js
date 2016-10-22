6849cf2866f51893c6f04b6a4fe5e6d05179d7ce
ssowemim <ssowemim@ualberta.ca>
2016-10-21 23:27:28 -0600

    refactoring code base

--- app-promise.js
+++ app-promise.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -12,3 +13,21 @@ const argv = yargs
 .help()
 .alias('help', 'h')
 .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl=`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS') {
+    throw new Error('Unable to find that address.');
+  }
+
+  console.log(response.data);
+}).catch((e) => {
+  if (e.code === 'ENOTFOUND'){
+    console.log('Unable to connect to api servers');
+  } else {
+    console.log(e.message);
+  }
+
+});
