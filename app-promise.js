ec35c731e32a48ee7f40872a9f2fb9b95e2da3aa
ssowemim <ssowemim@ualberta.ca>
2016-10-21 23:36:45 -0600

    creating weather app using promises

--- app-promise.js
+++ app-promise.js
@@ -22,7 +22,16 @@ axios.get(geocodeUrl).then((response) => {
     throw new Error('Unable to find that address.');
   }
 
-  console.log(response.data);
+var lat=response.data.results[0].geometry.location.lat;
+var lng = response.data.results[0].geometry.location.lng;
+var weatherUrl = `https://api.darksky.net/forecast/89b418137e156f76398b310bae0d8562/${lat},${lng}`;
+
+console.log(response.data.results[0].formatted_address);
+return axios.get(weatherUrl);
+}).then((response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's Currently ${temperature}. It feels like ${apparentTemperature}.`);
 }).catch((e) => {
   if (e.code === 'ENOTFOUND'){
     console.log('Unable to connect to api servers');
